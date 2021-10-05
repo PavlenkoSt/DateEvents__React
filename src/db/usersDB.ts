a4a795384fdf34552b67153dff763257a59f7967
Stanislav <verbatim8872@gmail.com>
2021-10-05 20:10:58 +0300

    mock users db

---
+++ src/db/usersDB.ts
@@ -0,0 +1,9 @@
+import { UserType } from "../models/UserType"
+
+const usersDB: UserType[] = [
+    { id: 1, username: 'user', password: '123' },
+    { id: 2, username: 'admin', password: '123' },
+    { id: 3, username: 'user2', password: '123' }
+]
+
+export default usersDB
