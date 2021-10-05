f4fe07bafa841dd6df3b4eeed18a11e45876df85
Stanislav <verbatim8872@gmail.com>
2021-10-05 20:34:26 +0300

    add login form

--- src/store/reducers/auth/index.ts
+++ src/store/reducers/auth/index.ts
@@ -2,13 +2,19 @@ import { AuthActionTypeEnum, AuthActionTypes, AuthStateType } from './types'
 
 
 const initialState: AuthStateType = {
-    isAuth: false
+    isAuth: false,
+    isLoading: false,
+    error: ''
 }
 
 const authReducer = (state = initialState, action: AuthActionTypes): AuthStateType => {
     switch(action.type){
         case AuthActionTypeEnum.SET_IS_AUTH: 
             return { ...state, isAuth: action.payload }
+        case AuthActionTypeEnum.SET_IS_LOADING: 
+            return { ...state, isLoading: action.payload }
+        case AuthActionTypeEnum.SET_ERROR: 
+            return { ...state, error: action.payload }
         default: 
             return state
     }
--- src/store/reducers/auth/types.ts
+++ src/store/reducers/auth/types.ts
@@ -1,15 +1,31 @@
 export type AuthStateType = {
     isAuth: boolean
+    isLoading: boolean
+    error: string
 }
 
 export enum AuthActionTypeEnum {
-    SET_IS_AUTH = 'SET_IS_AUTH'
+    SET_IS_AUTH = 'SET_IS_AUTH',
+    SET_IS_LOADING = 'SET_IS_LOADING',
+    SET_ERROR = 'SET_ERROR'
 }
 
-export type SetIsAuth = {
+export type SetIsAuthAction = {
     type: AuthActionTypeEnum.SET_IS_AUTH,
     payload: boolean
 }
 
+export type SetIsLoadingAction = {
+    type: AuthActionTypeEnum.SET_IS_LOADING,
+    payload: boolean
+}
+
+export type SetErrorAction = {
+    type: AuthActionTypeEnum.SET_ERROR,
+    payload: string
+}
+
 export type AuthActionTypes = 
-    SetIsAuth
+    SetIsAuthAction | 
+    SetIsLoadingAction |
+    SetErrorAction
